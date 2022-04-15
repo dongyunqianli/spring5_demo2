60e2644b173ded0efe877bd7ad87ea9bbcb4fb4b
dongyunqianli <2217322445@qq.com>
2022-04-15 16:51:10 +0800

    Initial commit

---
+++ src/com/atguigu/spring5/bean/Orders.java
@@ -0,0 +1,28 @@
+package com.atguigu.spring5.bean;
+
+public class Orders {
+
+
+    //无参构造器
+    public Orders(){
+        System.out.println("第一步，执行无参数构造创建bean实例");
+    }
+
+
+    private String oname;
+    public void setOname(String name){
+        this.oname=oname;
+        System.out.println("第二步骤，调用set方法设置属性值");
+    }
+
+
+    //创建执行的初始化的方法
+    public void initMethod(){
+        System.out.println("第三步，执行初始化的方法");
+    }
+
+    //创建执行的销毁的方法
+    public void destroyMehotd(){
+        System.out.println("第五步， 执行 销毁的方法");
+    }
+}
